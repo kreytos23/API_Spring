77a463024ba6d7d835dbcc80dcf4acd5c3e5724e
Cesar Martin <kreytos.pavi10@gmail.com>
2020-10-25 23:08:18 -0600

    conecion a base de datos

--- src/main/java/web/controller/CesarMarketApplication.java
+++ src/main/java/web/controller/CesarMarketApplication.java
@@ -1,4 +1,4 @@
-package com.Cesar_Market;
+package web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class CesarMarketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CesarMarketApplication.class, args);
 	}
-
 }
