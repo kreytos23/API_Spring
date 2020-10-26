cff0968c44d0c670a7bdaa7babe7e91f9b183096
Cesar Martin <kreytos.pavi10@gmail.com>
2020-10-25 21:20:14 -0600

    primer commit del proyecto

---
+++ src/main/java/com/Cesar_Market/CesarMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Cesar_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarMarketApplication.class, args);
+	}
+
+}
